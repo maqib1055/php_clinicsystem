2df9a2cfb819a8c708591b76c4374e6e01d74552
aqib <maqib1055@gmail.com>
2026-03-14 22:54:13 +0500

    dashboard

--- dashboard.php
+++ dashboard.php
@@ -6,6 +6,10 @@ if(!isset($_SESSION['uid'])){
     header('Location: index.php');
 }
 
+$total_users = mysqli_fetch_array(mysqli_query($conn, "select count(*) from users"));
+$total_doctors = mysqli_fetch_array(mysqli_query($conn, "select count(*) from doctors"));
+$total_opd = mysqli_fetch_array(mysqli_query($conn, "select count(*) from opd"));
+
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -26,6 +30,7 @@ if(!isset($_SESSION['uid'])){
                     <div class="card-body">
                         <div class="card-text">
                             <a href="#" class="btn btn-dark">Users</a>
+                            <br> <p><?= $total_users ?></p>
                         </div>
                     </div>
                 </div>
@@ -35,6 +40,7 @@ if(!isset($_SESSION['uid'])){
                     <div class="card-body">
                         <div class="card-text">
                             <a href="#" class="btn btn-dark">Doctors</a>
+                            <br> <p><?= $total_doctors ?></p>
                         </div>
                     </div>
                 </div>
@@ -44,6 +50,7 @@ if(!isset($_SESSION['uid'])){
                     <div class="card-body">
                         <div class="card-text">
                             <a href="#" class="btn btn-dark">Appointments</a>
+                            <br> <p><?= $total_opd ?></p>
                         </div>
                     </div>
                 </div>
