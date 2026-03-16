5d10e04ff0d57af803c738c169f3c59ff4f0a4fd
aqib <maqib1055@gmail.com>
2026-03-16 17:01:56 +0500

    doctor delete

---
+++ edit_doctor.php
@@ -0,0 +1,82 @@
+<?php
+
+include 'connect.php';
+
+if(!isset($_SESSION['uid'])){
+    header('Location: index.php');
+}
+
+if(isset($_GET['id'])){
+
+    $id = $_GET['id'];
+    $query = "select * from doctors where doctorid = '$id'";
+    $result = mysqli_query($conn, $query);
+    $doctors = mysqli_fetch_array($result);
+   
+
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Update Doctor</title>
+</head>
+<body>
+    <?php include 'header.php' ?>
+
+    <div class="container">
+        <form action="edit_doctor.php" method="post">
+            <div class="form-group mb-2">
+                <input type="hidden" name="doctorid" value="<?= $doctors['doctorid'] ?>">
+                <label for="">Name</label>
+                <input type="text" name="name" value="<?= $doctors['name']  ?>" class="form-control">
+            </div>
+            <div class="form-group mb-2">
+                <label for="">Education</label>
+                <input type="text" name="education" value="<?= $doctors['education']  ?>" class="form-control">
+            </div>
+            <div class="form-group mb-2">
+                <label for="">Speciality</label>
+                <input type="text" name="speciality" value="<?= $doctors['speciality']  ?>" class="form-control">
+            </div>
+            <div class="form-group mb-2">
+                <label for="">Days/Timing</label>
+                <input type="text" name="days_timing" value="<?= $doctors['day_timing']  ?>" class="form-control">
+            </div>
+            <div class="form-group mb-2">
+                <label for="">Fee</label>
+                <input type="number" name="fee" value="<?= $doctors['fee']  ?>" class="form-control">
+            </div>
+            <div class="form-group mb-2">
+                <input type="submit" name="btnUpdate" class="btn btn-primary">
+            </div>
+        </form>
+        
+    </div>
+    <?php } ?>
+</body>
+</html>
+
+<?php
+if(isset($_POST['btnUpdate'])){
+
+    $doctorid       = $_POST['doctorid'];
+    $name           = $_POST['name'];
+    $education      = $_POST['education'];
+    $speciality     = $_POST['speciality'];
+    $days_timing    = $_POST['days_timing'];
+    $fee            = $_POST['fee'];
+
+    $query = mysqli_query($conn, "UPDATE `doctors` SET `name`='$name',
+    `education`='$education',`speciality`='$speciality',
+    `day_timing`='$days_timing',`fee`='$fee' WHERE doctorid = '$doctorid'");
+
+    if($query == true){
+        echo "<script> alert('doctor updated'); window.location.href='view_doctor.php'; </script>";
+    }else{
+        echo "<script> alert('doctor not updated') </script>";
+    }
+}
+?>
--- view_doctor.php
+++ view_doctor.php
@@ -6,6 +6,18 @@ if(!isset($_SESSION['uid'])){
     header('Location: index.php');
 }
 
+if(isset($_GET['delid'])){
+    $id = $_GET['delid'];
+   // $query = "delete from doctors where doctorid = '$id'";
+    $query = "update doctors set status = 0 where doctorid = '$id'";
+    if(mysqli_query($conn, $query)){
+        echo "<script> alert('doctor deleted'); window.location.href='view_doctor.php' </script>";
+
+    }else{
+        echo "<script> alert('doctor not delete') </script>";
+    }
+}
+
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -28,12 +40,13 @@ if(!isset($_SESSION['uid'])){
                 <th>Speciality</th>
                 <th>Days/Timing</th>
                 <th>Fee</th>
+                <th>Status</th>
                 <th>Action</th>
             </tr>
          </thead>
          <tbody>
             <?php
-            $query = "select * from doctors";
+            $query = "select * from doctors where status = 1";
             $result = mysqli_query($conn, $query);
             if(mysqli_num_rows($result)>0){
                 while($doctors = mysqli_fetch_array($result)){
@@ -46,8 +59,15 @@ if(!isset($_SESSION['uid'])){
                         <td><?= $doctors['day_timing'] ?></td>
                         <td><?= $doctors['fee'] ?></td>
                         <td>
+                            <?php  if($doctors['status'] == 1){
+                                echo "<span class='badge bg-success'>Active</span>";
+                            }else{
+                                 echo "<span class='badge bg-danger'>Inactive</span>";
+                            } ?>
+                        </td>
+                        <td>
                             <a href="edit_doctor.php?id=<?= $doctors['doctorid'] ?>"><span class="fa fa-edit text-info"></span></a>
-                            <a href="view_doctor.php?id=<?= $doctors['doctorid'] ?>"><span class="fa fa-trash text-danger"></span></a>
+                            <a href="view_doctor.php?delid=<?= $doctors['doctorid'] ?>" onClick="return confirm('Are you sure you want to delete?')"  ><span class="fa fa-trash text-danger"></span></a>
                         </td>
                     </tr>
                     <?php
